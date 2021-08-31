b6da23677eacbab09db709f264b8b6cde9f0d440
MariaCamila <mariacmaza7@gmail.com>
2021-08-30 20:13:22 -0500

    Tipos de datos, conversiones de tipos de datos y concatenaciones

--- clase_uno.py
+++ clase_uno.py
@@ -5,4 +5,114 @@ Created on Mon Aug 30 18:53:28 2021
 @author: Maria Camila
 """
 
+print('Hello World')
 
+# Variables
+a = 3
+print(type(a))
+a = "Hola"
+print(type(a))
+a = 3.5
+print(type(a))
+a = True
+print(type(a))
+
+
+# Operaciones
+
+# Suma
+
+a = 5
+b = 2
+c = a + b
+print(c)
+
+
+#  Resta
+
+a = 5
+b = 2
+c = a - b
+print(c)
+
+# Multiplicacion
+
+a = 5
+b = 2
+c = a * b
+print(c)
+
+# Division
+
+a = 5
+b = 2
+c = a / b
+print(c)
+
+a = 5
+b = 2
+c = a // b
+print(c)
+
+# Potencia
+
+a = 5
+b = 2
+c = a ** b
+print(c)
+
+# Raiz
+
+a = 9
+c = a ** (1/3)
+print(c)
+
+# import math
+# raiz = math.sqrt(25)
+# print(raiz)
+
+# Tipos de datos
+
+# String str
+a = "Hola Mundo"
+a = 'Hola Mundo'
+b = "I can't do it"
+c = 'Alias "Maria"'
+
+# Entero int
+a = 5
+
+# Decimal float
+a = 5.6
+
+# Booleano bool
+x = True
+y = False
+
+# Conversiones entre tipos de datos
+
+# Convertir de x a entero
+
+a = '3'
+y = int(a)
+print(y)
+print(type(y))
+
+# Convertir de x a decimal
+
+a = '3'
+y = float(a)
+print(y)
+print(type(y))
+
+# Convertir de x a string
+
+a = 3
+y = str(a)
+print(y)
+print(type(y))
+
+# Concatenaciones
+a = 'hola'
+b = 'mundo'
+c = a + ' ' + b
